2a43bcfd698d58374d364d9b6310548cea4dd711
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2021-03-24 00:42:03 +0000

    Ship, bullet and code

---
+++ Triangulon/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject target;
+
+    public Vector3 targetVec;
+
+    public float speed = 5f;
+
+    void Awake()
+    {
+        targetVec = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, targetVec, step);
+    }
+}
---
+++ Triangulon/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ship;
+    public GameObject bullet;
+
+    public Vector3 pos;
+    public Quaternion rot;
+
+    public bool waiting = false;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        pos = new Vector3(ship.transform.position.x, (ship.transform.position.y + 1f), ship.transform.position.z);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ship.transform.Rotate(0, 0, 3);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ship.transform.Rotate(0, 0, -3);
+        }
+
+        if (Input.GetKey("space") && waiting == false)
+        {
+            rot.Set(0, 0, ship.transform.rotation.z, 1);
+            Instantiate(bullet, ship.transform.position + (transform.up * 1), ship.transform.rotation * Quaternion.Euler (0f, 0f, 0f));
+            StartCoroutine("wait");
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            ship.transform.position += transform.up * 0.06f;
+        }
+    }
+
+    IEnumerator wait()
+    {
+        waiting = true;
+        yield return new WaitForSeconds(0.5f);
+        waiting = false;
+    }
+
+    //Change the Quaternion values depending on the values of the ship
+    private static Quaternion Change(float x, float y, float z)
+    {
+        Quaternion rot = new Quaternion();
+        rot.Set(x, y, z, 1);
+        //Return the new Quaternion
+        return rot;
+    }
+}
